3e7ccf2ddbc772133f8c1b5d6cfe696e92d40214
heynar-max <heynar76@hotmail.com>
2025-08-02 17:15:26 -0500

    Preparación de archivos y directorios

---
+++ src/HesodoksApp.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const HesodoksApp = () => {
+    return (
+        <View>
+            <Text> hola </Text>
+        </View>
+    )
+}
