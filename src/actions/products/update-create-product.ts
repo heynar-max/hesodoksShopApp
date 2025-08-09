455a8ed481fd87888c5ab7aadd3ac246cafdbc58
heynar-max <heynar76@hotmail.com>
2025-08-08 19:57:38 -0500

    Actualizar producto

---
+++ src/actions/products/update-create-product.ts
@@ -0,0 +1,55 @@
+import { isAxiosError } from 'axios';
+import { Product } from '../../domain/entities/product';
+import { hesoApi } from '../../config/api/hesoApi';
+
+
+
+export const updateCreateProduct = ( product: Partial<Product> ) => {
+
+    product.inStock = isNaN( Number(product.inStock)) ? 0 : Number(product.inStock);
+    product.price = isNaN( Number(product.price)) ? 0 : Number(product.price);
+
+
+    if ( product.id && product.id !== 'new') {
+        return updateProduct(product);
+    }
+
+
+    }
+
+
+    const prepareImages = ( images: string[] ) => {
+
+    // Todo: revisar los FILES
+
+    return images.map(
+        image => image.split('/').pop()
+    )
+
+    }
+
+    //TODO: revisar si viene el usuario
+    const updateProduct = async (product: Partial<Product>) => {
+    const { id, ProductImage = [], ...rest  } = product;
+
+    try {
+        const checkedImages = prepareImages(ProductImage);
+    
+        const { data } = await hesoApi.patch(`/products/${id}`, {
+        images: checkedImages,
+        ...rest
+        })
+
+        return data;
+        
+    } catch (error) {
+
+        if ( isAxiosError(error) ) {
+        console.log(error.response?.data);
+        }
+        
+        throw new Error('Error al actualizar el producto');
+
+    }
+
+    }
